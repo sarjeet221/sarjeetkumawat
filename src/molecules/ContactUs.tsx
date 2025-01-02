a887d1be94cb3c06f50ab4978281f6b92068a7de
sarjeet <sarjeetkumawat221@gmail.com>
2025-01-03 00:13:58 +0530

    thi is lasr form 02 jan

--- src/molecules/ContactUs.tsx
+++ src/molecules/ContactUs.tsx
@@ -1,30 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
 import address from '@/assets/address.png'
 import phoe from '@/assets/phone.png'
 import email from '@/assets/email.png'
 import Image from 'next/image'
 import PrimaryBtn from '@/atomes/PrimaryBtn'
+import { useForm } from 'react-hook-form';
+
 
 export default function ContactUs() {
+    const [responsefromserver,setresponsefromserver] = useState<any>();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    async function sendData(data: object) {
+        try {
+            const response = await fetch("http://localhost:4000/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const result = await response.text();
+            console.log("Request data:", data);
+            console.log("Response from server:", result);
+            setresponsefromserver(result)
+
+        } catch (error: any) {
+            console.error("Error occurred while sending data:", error.message);
+        }
+    }
     return (
 
         <div className='mt-12 w-full lg:w-7/12 bg-[white-200] text-center m-auto'>
-            <div  className='shadow-[0_0_77px_-4px_#0000001a] rounded py-6 mx-2'>
+            <div className='shadow-[0_0_77px_-4px_#0000001a] rounded py-6 mx-2'>
                 <h4 className="text-[28px] text-blue-400 font-semibold relative text-center flex justify-center
                     after:content-[''] after:block after:h-[2px] after:w-[100px] after:bg-green-300 after:absolute  after:bottom-0">
                     Contact Me
                 </h4>
-                <form action="" className='grid sml:grid-cols-2 gap-5 lg:gap-8 lg:mt-12 mt-7 p-5 w-full'>
-                    <input type="text" placeholder='Name' className='smlmax:w-full  boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold  ' />
-                    <input type="text" placeholder='Last Name' className='smlmax:w-full  boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold  ' />
-                    <input type="text" placeholder='Mobile' className='smlmax:w-full  boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold  ' />
-                    <input type="email" placeholder='Email' className='smlmax:w-full  boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold  ' />
-                    <textarea name="message" id="" cols={5} className='smlmax:mb-4 smlmax:w-full sml:col-span-2 boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold' placeholder='Message'></textarea>
 
-                    {/* <input type="submit" /> */}
-                    <PrimaryBtn>submit</PrimaryBtn>
+                <form className="grid sml:grid-cols-2 gap-5 lg:gap-8 lg:mt-12 mt-7 p-5 w-full">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        {...register('name', { required: 'Name is required' })}
+                        className="smlmax:w-full boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold"
+                    />
+
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        {...register('lastName', { required: 'Last Name is required' })}
+                        className="smlmax:w-full boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold"
+                    />
+
+                    <input
+                        type="text"
+                        placeholder="Mobile"
+                        {...register('mobile', {
+                            required: 'Mobile number is required',
+                            pattern: {
+                                value: /^[0-9]{10}$/,
+                                message: 'Mobile number must be 10 digits',
+                            },
+                        })}
+                        className="smlmax:w-full boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold"
+                    />
+
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                                value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                                message: 'Invalid email format',
+                            },
+                        })}
+                        className="smlmax:w-full boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold"
+                    />
+
+                    <textarea
+                        placeholder="Message"
+                        {...register('message', { required: 'Message is required' })}
+                        cols={5}
+                        className="ssmlmax:w-full sml:col-span-2 boder-0 outline-none border-b border-[#ddd] lg:pb-5 pb-3 lg:placeholder:text-xl placeholder:text-sm placeholder:font-semibold"
+                    ></textarea>
+
+                    <div className='sml:col-span-2 text-left'>
+                        {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
+                        {errors.lastName && <p className="text-red-500 text-sm">{errors?.lastName?.message}</p>}
+                        {errors.mobile && <p className="text-red-500 text-sm">{errors.mobile.message}</p>}
+                        {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
+                        {errors.message && <p className="text-red-500 text-sm">{errors.message.message}</p>}
+                        {responsefromserver && <p className="text-green-500 text-sm">{responsefromserver}</p>}
+                        
+                    </div>
+                    <PrimaryBtn event={handleSubmit(sendData)}>submit</PrimaryBtn>
                 </form>
             </div>
+
+
             <div className='grid sml:grid-cols-3 mt-12 gap-y-8 px-1 '>
 
                 <div>
