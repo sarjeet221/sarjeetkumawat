27000239faa3fe0c1f55c6a1eee038b17e229d1e
sarjeet <sarjeetkumawat221@gmail.com>
2025-01-04 19:03:14 +0530

    form like to blob successfully

--- pages/api/save-data.js
+++ pages/api/save-data.js
@@ -4,14 +4,15 @@ import { put } from '@vercel/blob';
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         try {
+            const { name, ...data } = req.body;
             const bodyData = JSON.stringify(req.body);
             const bufferData = Buffer.from(bodyData);
             
-            const { url } = await put('contact/form-data.json', bufferData, {
+            const { url } = await put(`contact/${name}.json`, bufferData, { 
                 access: 'public', 
                 contentType: 'application/json',
             });
-            res.status(200).json({ message: 'Data saved successfully', url });
+            res.status(200).json({ message: 'Message send successfully', url });
         } catch (error) {
             console.error('Error saving data:', error);
             res.status(500).json({ message: 'Error saving data', error });
